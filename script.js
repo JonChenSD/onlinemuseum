0947abc9a2f66d79fe12e7b40fb6215951936918
POaTatOo <52507937+POaTatOo@users.noreply.github.com>
2020-04-10 16:50:39 -0700

    Hover caption functionality

--- script.js
+++ script.js
@@ -1,13 +1,50 @@
-var text = ["Welcome to the Museum", "Feel free to explore"];
 var counter = 0;
 var elem = document.getElementById("changeText");
 var inst = setInterval(change, 4000);
+var temp = 0;
 
+
+
+var text = ["Welcome to the Museum", "Feel free to explore"];
+
+elem.innerHTML = text[counter];
+counter ++;
+console.log('counter is increased');
+console.log(counter);
 function change() {
+  console.log('hello');
   elem.innerHTML = text[counter];
   counter++;
   if (counter >= text.length) {
     counter = 0;
+    console.log('counter');
     // clearInterval(inst); // uncomment this if you want to stop refreshing after one cycle
   }
 }
+
+function newquote(x) {
+  console.log('hello');
+  clearInterval(inst);
+  elem.innerHTML = x;
+}
+
+function newqdone() {
+  inst = setInterval(change,4000);
+  if(counter == 0)
+    {
+      temp = ((text.length) - 1);
+      console.log(temp);
+      elem.innerHTML = text[temp];
+      //elem.innerHTML = text[counter-1];
+      console.log(text.length);
+    }
+  else
+    {
+      elem.innerHTML = text[counter-1];
+      console.log('else statement');
+    }
+    
+  
+}
+
+
